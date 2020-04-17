d0cb2e9a38681aa3329309653854e99bc889d4f7
Javernaut <javernaut@gmail.com>
2020-04-17 19:09:13 +0300

    Adding an AsyncTask for simple threading

--- app/src/main/java/com/javernaut/networking/MainActivity.java
+++ app/src/main/java/com/javernaut/networking/MainActivity.java
@@ -1,14 +1,18 @@
 package com.javernaut.networking;
 
+import androidx.annotation.MainThread;
+import androidx.annotation.WorkerThread;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.javernaut.networking.adapter.GithubersAdapter;
 import com.javernaut.networking.model.Githuber;
 
+import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,9 +26,34 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        // Starting the background task
+        new InternetRequestTask().execute();
     }
 
+    @MainThread
     private void applyGithubers(List<Githuber> githubers) {
         recyclerView.setAdapter(new GithubersAdapter(githubers));
+    }
+
+    @WorkerThread
+    private List<Githuber> executeRequest() {
+        return Collections.emptyList();
+    }
+
+    // The class for a background task
+    private class InternetRequestTask extends AsyncTask<Void, Void, List<Githuber>> {
+
+        // Method will be called in background thread
+        @Override
+        protected List<Githuber> doInBackground(Void... voids) {
+            return executeRequest();
+        }
+
+        // Method will be called in main thread after the doInBackground() has finished
+        @Override
+        protected void onPostExecute(List<Githuber> githubers) {
+            applyGithubers(githubers);
+        }
     }
 }
