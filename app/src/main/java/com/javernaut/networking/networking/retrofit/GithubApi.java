fb69218f444b2d1435b8c86511dfa1a60045f4bd
Javernaut <javernaut@gmail.com>
2020-04-18 12:43:30 +0300

    Adding 'get followers of' request to Retrofit implementation

--- app/src/main/java/com/javernaut/networking/networking/retrofit/GithubApi.java
+++ app/src/main/java/com/javernaut/networking/networking/retrofit/GithubApi.java
@@ -6,10 +6,13 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface GithubApi {
 
     @GET("/users")
     Call<List<Githuber>> getUsers();
 
+    @GET("/users/{user}/followers")
+    Call<List<Githuber>> getFollowersOf(@Path("user") String user);
 }
